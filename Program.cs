1862ec71182e727cffb098044644e7aed7728a2e
timsnyder <snydertimothym@gmail.com>
2018-04-05 23:22:59 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime startTime = DateTime.Now;
+
+            DateTime endTime = DateTime.Now.AddSeconds();
+
+            TimeSpan span = endTime.Subtract(startTime);
+            Console.WriteLine("Time Difference (seconds): " + span.Seconds);
+            Console.WriteLine("Time Difference (minutes): " + span.Minutes);
+            Console.WriteLine("Time Difference (hours): " + span.Hours);
+            Console.WriteLine("Time Difference (days): " + span.Days);
+        }
+    }
+}
